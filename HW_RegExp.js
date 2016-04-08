30be297e25be5bf0ac1d0e4a7146908f3a9d216c
Olimpiada Lysenko <olimpiadalys@gmail.com>
2016-04-08 15:59:54 +0300

    Add method
    Add method replace for RegExp

--- HW_RegExp.js
+++ HW_RegExp.js
@@ -1,5 +1,22 @@
-/^0x[a-f]*?\d*?[a-f]*?$/gm 
+/^0x([a-f]|\d)*?$/gm 
 //0xfff
 //0xab34209ab
 //0x00001
 
+/^0o\d*$/gm
+//0o657575765
+//0o12341230
+//0o0000
+
+/^0b[01]+$/gm
+//0b0000001111
+//0b1111111001
+//0b1101010101
+
+var str = "Hello, my name is ${1+1}"
+var result = str.replace(/\$\{([\d\/\*\-\+]+?)\}/g, function(all, match)
+{
+    return eval(match)
+});
+
+console.log(result)
